8c5b11faa57c5a0e52b5740daad02d66f982c800
Tom Wadeson <tom@tomwadeson.com>
2019-08-14 13:42:02 +0100

    Demo of pattern functors from functor composition

--- src/main/scala/schemes/patterns/ListF.scala
+++ src/main/scala/schemes/patterns/ListF.scala
@@ -3,6 +3,8 @@ package schemes
 package patterns
 
 import cats.Functor
+import cats.data.Nested
+import cats.implicits._
 
 sealed trait ListF[+A, +B] extends Product with Serializable
 
@@ -53,6 +55,52 @@ object ListF {
     val range: Coalgebra[ListF[Int, ?], Int] =
       n => if (n > 0) ListF.Cons(n, n - 1) else ListF.Nil
   }
+
+  object compose {
+
+    object list {
+
+      type ListF[A, B] = Nested[Option, (A, ?), B]
+      type List[A] = Fix[ListF[A, ?]]
+
+      def cons[A](head: A, tail: List[A]): List[A] =
+        Fix(Nested(Some((head, tail))))
+
+      def nil[A]: List[A] = Fix(Nested(None))
+
+      def product[A](implicit N: Numeric[A]): Algebra[ListF[A, ?], A] =
+        fa =>
+          fa.value match {
+            case Some((head, tail)) => N.times(head, tail)
+            case None               => N.one
+        }
+
+      val range: Coalgebra[ListF[Int, ?], Int] =
+        n => if (n > 0) Nested(Some((n, n - 1))) else Nested(None)
+    }
+
+    object nel {
+
+      type NonEmptyListF[A, B] = Nested[(A, ?), Option, B]
+      type NonEmptyList[A] = Fix[NonEmptyListF[A, ?]]
+
+      def cons[A](head: A, tail: NonEmptyList[A]): NonEmptyList[A] =
+        Fix(Nested((head, Some(tail))))
+
+      def one[A](a: A): NonEmptyList[A] =
+        Fix(Nested((a, none)))
+
+      def product[A](implicit N: Numeric[A]): Algebra[NonEmptyListF[A, ?], A] =
+        fa =>
+          fa.value match {
+            case (head, Some(tail)) => N.times(head, tail)
+            case (head, None)       => head
+        }
+
+      val range: Coalgebra[NonEmptyListF[Int, ?], Int] =
+        n => if (n > 1) Nested((n, (n - 1).some)) else Nested((1, none))
+    }
+  }
 }
 
 object ListExample extends App {
@@ -98,4 +146,42 @@ object ListExample extends App {
     hylo(algebras.product[Int], coalgebras.range)(n)
 
   println(factorial(5)) // 120
+
+  {
+    import ListF.compose.list._
+
+    val list: List[Int] =
+      cons(1, cons(2, cons(3, cons(4, cons(5, nil)))))
+
+    println(list)
+    println(list.cata(product))
+
+    val `3!` = hylo(product[Int], range)(3)
+    println(`3!`)
+
+    // Output:
+    // List(5, 4, 3, 2, 1)
+    // 120
+    // Fix(Nested(Some((1,Fix(Nested(Some((2,Fix(Nested(Some((3,Fix(Nested(Some((4,Fix(Nested(Some((5,Fix(Nested(None))))))))))))))))))))))
+    // 120
+    // 6
+  }
+
+  {
+    import ListF.compose.nel._
+
+    val nel: NonEmptyList[Int] =
+      cons(1, cons(2, cons(3, cons(4, one(5)))))
+
+    println(nel)
+    println(nel.cata(product))
+
+    val `3!` = hylo(product[Int], range)(3)
+    println(`3!`)
+
+    // Output:
+    // Fix(Nested((1,Some(Fix(Nested((2,Some(Fix(Nested((3,Some(Fix(Nested((4,Some(Fix(Nested((5,None)))))))))))))))))))
+    // 120
+    // 6
+  }
 }
